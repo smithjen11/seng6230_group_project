82632c35a079b98619cf3c6a8443e163b1820bcf
Jennifer Smith <jennoffur2@aol.com>
2017-04-14 17:36:02 -0400

    added spaces to applicaiton title and added users to db to test login

--- point_of_sale_app/db/seeds.rb
+++ point_of_sale_app/db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.first_or_create(email: 'manager@business.com', password: 'password1', password_confirmation: 'password1')
+User.first_or_create(email: 'employee@business.com', password: 'password2', password_confirmation: 'password2')
